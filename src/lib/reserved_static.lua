8d2fd67d3c0580d3f0d3e82ab340bc237844b978
ThistleSifter <78799201+ThistleSifter@users.noreply.github.com>
2023-05-22 18:49:01 +0930

    Add lib/reserved_static.lua

---
+++ src/lib/reserved_static.lua
@@ -0,0 +1,29 @@
+local utils = require("lib.utils")
+local helper = require("lib.helper")
+local reserved_static = {}
+
+function reserved_static.__class(class)
+    return class.Methods and utils.copy_table(class.Methods) or {}
+end
+
+function reserved_static.__static(class)
+    return class.StaticMethods and utils.copy_table(class.StaticMethods) or {}
+end
+  
+function reserved_static.__propget(class)
+    return helper.create_getter_reflection(class)
+end
+
+function reserved_static.__propset(class)
+    return helper.create_setter_reflection(class)
+end
+
+function reserved_static.__disabled(class)
+    return class.Disabled and utils.copy_table(class.Disabled) or {}
+end
+
+function reserved_static.__metamethods(class)
+    return class.MetaMethods and utils.copy_table(class.MetaMethods) or {}
+end
+
+return reserved_static
