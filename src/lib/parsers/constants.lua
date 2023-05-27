d0c674637aeba7b47cada40e1007361adbeaa849
ThistleSifter <78799201+ThistleSifter@users.noreply.github.com>
2023-05-28 01:34:56 +0930

    Update constants.lua

--- src/lib/parsers/constants.lua
+++ src/lib/parsers/constants.lua
@@ -1,20 +1,7 @@
-
-return function(result, dest, mod_name)
-    local function assert_valid_name(name, err_name, err_path)
-        if not string.match(name, "^%u+") and not string.match(name, "^%u[%u_]-%u$") then
-            error("Constant " .. err_name .. " can only contain uppercase letters and underscores and cannot start or end with an underscore, '" .. tostring(name) .. "' given (" .. mod_name .. (err_path or "")  .. ")", 0)
-        end
-    end
-
+return function(result, target)
     for k, v in pairs(result) do
-        assert_valid_name(k, "group names")
         for kk, vv in pairs(v) do
-            assert_valid_name(kk, "names", "." .. k)
-            local const = k .. "_" .. kk
-            if dest[const] ~= nil then
-                error("A constant named '" .. const .. "' already exists and cannot be overridden (" .. mod_name .. "." .. k .. "." .. kk .. ")" , 0)
-            end
-            dest[const] = vv
+            target[k .. "_" .. kk] = vv
         end
     end
 end
