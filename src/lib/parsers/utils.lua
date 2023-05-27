633b245ddeab1b7eb9512896ff5cf333aca2b376
ThistleSifter <78799201+ThistleSifter@users.noreply.github.com>
2023-05-28 01:36:47 +0930

    Update utils.lua

--- src/lib/parsers/utils.lua
+++ src/lib/parsers/utils.lua
@@ -1,10 +1,5 @@
-local utils = require("lib.utils")
-
-return function(result, dest, mod_name)
+return function(result, target)
     for k, v in pairs(result) do
-        if dest[k] ~= nil then
-            error("Utils key '" .. tostring(k) .. "' already exists and cannot be overwritten (" .. mod_name .. "." .. tostring(k) .. ")", 0)
-        end
-        dest[utils.copy_table(k)] = utils.copy_table(v)
+        target[k] = v
     end
 end
