71b96d07deca245f2dad3ad55132f779801a851e
ThistleSifter <78799201+ThistleSifter@users.noreply.github.com>
2023-05-28 03:35:24 +0930

    Create constants.lua

---
+++ src/lib/validators/constants.lua
@@ -0,0 +1,46 @@
+local function is_valid_name(name)
+    return string.match(name, "^%u+") or string.match(name, "^%u[%u_]-%u$") and true or false
+end
+
+local function err_type(name, expected, given)
+    return name .. " must be a " .. expected .. ", " .. given .. " given"
+end
+
+local function err_name(name_type, name)
+    return "Constant " .. name_type .. " can only contain uppercase letters and underscores and cannot start or end with an underscore, '" .. name .. "' given"
+end
+
+return function(result)
+    if type(result) ~= "table" then
+        return false, err_type("Constant definition", "table", type(result))
+    end
+
+    for k, v in pairs(result) do
+        if type(k) ~= "string" then
+            return false, err_type("Constant group names", "string", type(k)), tostring(k)
+        end
+
+        if not is_valid_name(k) then
+            return false, err_name("group names", k), k
+        end
+
+        local value_index = {}
+        for kk, vv in pairs(v) do
+            if type(kk) ~= "string" then
+                return false, err_type("Constant names", "string", type(kk)), k .. "." .. tostring(kk)
+            end
+
+            if not is_valid_name(kk) then
+                return false, err_name("group names", kk), k .. "." .. kk
+            end
+
+            if value_index[v] then
+                return false, "Each value within a constant group must be unique", k .. "." .. "kk"
+            end
+
+            value_index[v] = true
+        end
+    end
+
+    return true
+end
