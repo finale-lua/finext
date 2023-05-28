d2f598034135fd29172225ea8f1ba17e43f662f3
ThistleSifter <78799201+ThistleSifter@users.noreply.github.com>
2023-05-28 10:38:13 +0930

    Update loaders.lua

--- src/lib/loaders.lua
+++ src/lib/loaders.lua
@@ -1,5 +1,24 @@
 local loaders = {}
 
+local function error_suffix(path, sub_path)
+    sub_path = sub_path and ("[" .. sub_path .. "]") or ""
+    return " (" .. path .. sub_path .. ")"
+end
+
+local function create_proxy(t, path)
+    return setmetatable({}, {
+        __index = function(_, k)
+            return t[k]
+        end,
+        __newindex = function(_, k, v)
+            if t[k] ~= nil then
+                error("Duplicate entry, the key '" .. tostring(k) .. "' already exists and cannot be overwritten" .. error_suffix(path, k), 0)
+            end
+            t[k] = v
+        end,
+    })
+end
+
 local require_error_handler = errors.create_handler({
     custom = function(msg)
         if msg:match("module '[^']-' not found") then
@@ -18,10 +37,10 @@ local function try_require(mod_name, exclude_external_files)
     return require_error_handler(require, mod_name)
 end
 
-function loaders.create_lazy_loader(paths, validator, exclude_external_files)
+function loaders.create_lazy_loader(paths, key_validator, default_getter, parser, validator, exclude_external_files)
     return setmetatable({}, {
         __index = function(t, k)
-            if not validator.is_valid_key(k) then
+            if not key_validator(k) then
                 return nil
             end
 
@@ -29,18 +48,26 @@ function loaders.create_lazy_loader(paths, validator, exclude_external_files)
             for _, p in ipairs(paths) do
                 local full_path = p .. "." .. k
                 result = try_require(require, full_path, exclude_external_files)
+
                 if result then
                     break
                 end
             end
 
             if result == nil then
-                result = validator.get_default_value(k)
+                result = default_getter(k)
             end
 
             if result ~= nil then
-                result = validator.validate(result, k, full_path)
-                rawset(t, k, result)
+                if validator then
+                    local is_valid, err_msg, err_path = validator(result, k, t)
+                    if not is_valid then
+                        error(err_msg .. error_suffix(full_path, err_path), 0)
+                    end
+                end
+
+                result = parser(result, k)
+                t[k] = result
             end
 
             return result
@@ -48,14 +75,21 @@ function loaders.create_lazy_loader(paths, validator, exclude_external_files)
     })
 end
 
-function loaders.load_into(t, mod_name, paths, parser, exclude_external_files)
+function loaders.load_into(t, mod_name, paths, parser, validator, exclude_external_files)
     for _, p in ipairs(paths) do
         local full_path = p .. "." .. mod_name
         local result = try_require(require, full_path, exclude_external_files)
-        if result then
-            parser(result, t, full_path)
+        if result ~= nil then
+            if validator then
+                local is_valid, err_msg, err_path = validator(result)
+                if not is_valid then
+                    error(err_msg .. error_suffix(full_path, err_path), 0)
+                end
+            end
+            parser(result, create_proxy(t, full_path))
         end
     end
+
     return t
 end
 
