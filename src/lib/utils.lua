a4e20b5b5775074254ca90de99007ca23f0f5721
ThistleSifter <78799201+ThistleSifter@users.noreply.github.com>
2023-05-28 21:53:47 +0930

    Update utils.lua

--- src/lib/utils.lua
+++ src/lib/utils.lua
@@ -31,6 +31,35 @@ function utils.copy_table_no_meta(t)
     return copy
 end
 
+---Creates a lookup table from an array table  (ie turns values into keys and sets them to true).
+---@param t any[]
+---@return table
+function utils.create_lookup(t)
+    local tt = {}
+    for _, v in ipairs(t) do
+        tt[v] = true
+    end
+    return tt
+end
+
+---Copies all key/value pairs from t2 into t1, overriding any existing values.
+---@param t1 table
+---@param t2 table
+function utils.copy_into(t1, t2)
+    for k, v in pairs(t2) do
+        t1[k] = v
+    end
+end
+
+---Removes all keys from t1 that are present in t2
+---@param t1 table
+---@param t2 table
+function utils.remove_keys(t1, t2)
+    for k, _ in pairs(t2) do
+        t1[k] = nil
+    end
+end
+
 ---Splits a string by another string.
 ---@param str string
 ---@param separator? string Pattern. Defaults to any space character (%s)
@@ -42,17 +71,6 @@ function utils.split(str, separator)
         table.insert(t, s)
     end
     return t
-end
-
----Creates a lookup table from an array table  (ie turns values into keys and sets them to true).
----@param t any[]
----@return table
-function utils.create_lookup(t)
-    local tt = {}
-    for _, v in ipairs(t) do
-        tt[v] = true
-    end
-    return tt
 end
 
 return utils
