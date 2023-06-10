53a9ee2956d1475d6dfc65c780d5b3e809aa2e0b
ThistleSifter <78799201+ThistleSifter@users.noreply.github.com>
2023-06-10 23:50:56 +0930

    Update extension.lua

--- src/lib/validators/extension.lua
+++ src/lib/validators/extension.lua
@@ -2,11 +2,13 @@ local utils = require("lib.utils")
 local errors = require("lib.errors")
 local helper = require("lib.helper")
 
-local function find_collision(class, attrs, props)
-    for _, v in ipairs(attrs) do
-        for _, vv in ipairs(props) do
-            if class[v] and class[v][vv] then
-                return v, vv
+local function find_collision(class, check, skip, props)
+    for _, v in ipairs(check) do
+        if not skip[k] then
+            for kk, _ in ipairs(props) do
+                if class[v] and class[v][kk] then
+                    return v, kk
+                end
             end
         end
     end
@@ -14,7 +16,7 @@ end
 
 local find_finale_name_clash
 find_finale_name_clash = function(class_name, attrs, props)
-    local clash_attr, clash_name = find_collision(finale[class_name], attrs, props)
+    local clash_attr, clash_name = find_collision(finale[class_name], {"__class", "__static", "__propget", "__propset"}, attrs, props)
     if clash_attr then
         return class_name, clash_attr, clash_name
     end
@@ -45,7 +47,7 @@ return function(result, class_name, classes)
         end
     end
 
-    local parent
+    local dummy = {class_name}
 
     -- Check parent if xFX
     if helper.is_xfx_class_name(class_name) then
@@ -61,43 +63,64 @@ return function(result, class_name, classes)
             return false, "Unable to load extension '" .. class.Parent .. "' as parent of '" .. class_name .. "'", "Parent"
         end
 
-        parent = result.Parent
+        dummy.Parent = result.Parent
+        dummy.Base = classes[dummy.Parent].Base or dummy.Parent
     else
-        parent = helper.get_parent_class(helper.xfc_to_fc_class_name(class_name))
+        dummy.Parent = helper.get_parent_class(helper.xfc_to_fc_class_name(class_name))
 
-        if parent then
-            parent = helper.fc_to_xfc_class_name(parent)
+        if dummy.Parent then
+            dummy.Parent = helper.fc_to_xfc_class_name(dummy.Parent)
         end
     end
 
     -- Check disabled and build lookup table
-    local disabled = {}
+    dummy.Disabled = {}
     if result.Disabled then
         for _, v in ipairs(result.Disabled) do
             local is_valid, err = policy.is_valid_property_name(v)
             if not is_valid then
                 return false, err, "Disabled." .. tostring(v)
             end
-            disabled[v] = true
+            dummy.Disabled[v] = true
         end
     end
 
     -- Check valid property names and values
-    for _, attr in pairs({"Methods", "StaticMethods", "Properties"}) do
+    dummy.Methods = result.Methods
+    dummy.StaticMethods = result.StaticMethods
+    dummy.Properties = result.Properties
+
+    local finale_class = helper.fc_to_xfc_class_name(dummy.Base or class_name)
+    for attr, finale_attr in pairs({
+        Methods = utils.create_lookup({"__class"}),
+        StaticMethods = utils.create_lookup({"__static"}),
+        Properties = utils.create_lookup({"__propget", "__propset"}),
+    }) do
         if result[attr] then
             for k, _ in pairs(result[attr]) do
-                local is_valid = true
-                local err
-                is_valid, err = policy.is_valid_property_name(k)
-                if is_valid then
-                    if disabled[k] or (parent and classes[parent].Lookup.Disabled[k]) then
-                        is_valid = false
-                        err = "'" .. k .. "' is a disabled name and cannot be used for properties or methods"
-                    end
-                end
+                local is_valid, err = policy.is_valid_property_name(k)
                 if not is_valid then
                     return false, err, attr .. "." .. tostring(k)
                 end
+            end
+
+            local cl = dummy
+            while cl do
+                for k, _ in pairs(result[attr]) do
+                    if cl.Disabled[k] then
+                        return false, "'" .. k .. "' is a disabled name and cannot be used for properties or methods", attr .. "." .. k
+                    end
+                end
+                local clash_class, clash_attr, clash_name = find_clash(cl, {"Methods", "StaticMethods", "Properties"}, {[attr] = true}, result[attr])
+                if clash_class then
+                    return false, "Extension method/property name clash (" .. table.concat({class_name, attr, clash_name}, ".") .. " & " .. table.concat({clash_class, clash_attr, clash_name}, ".") .. ")"
+                end
+                cl = classes[cl.Parent]
+            end
+
+            local clash_class, clash_attr, clash_name = find_finale_name_clash(finale_class, finale_attr, result[attr])
+            if clash_class then
+                return false, "Extension method/property name clash with Finale class (" .. table.concat({class_name, attr, clash_name}, ".") .. " & " .. table.concat({clash_class, clash_attr, clash_name}, ".") .. ")"
             end
         end
     end
@@ -140,52 +163,6 @@ return function(result, class_name, classes)
                 if type(vv) ~= "function" then
                     return false, errors.bad_value_msg("Extension property descriptor attribute '" .. kk .. "'", "function", type(vv)), "Properties." .. k .. "." .. kk
                 end
-            end
-        end
-    end
-
-    -- Check method/property name collisions throughout class heirarchy
-    local find_extension_name_clash
-    find_extension_name_clash = function(class, class_name, parent, prop_types, prop)
-        local prop_clash = find_collision(class, prop_types, prop)
-        if prop_clash then
-            return class_name, prop_clash
-        end
-
-        if parent then
-            return find_extension_name_clash(classes[parent], parent, classes[parent].Parent, prop_types, prop)
-        end
-    end
-
-    local ext_attr = utils.create_lookup({"Methods", "StaticMethods", "Properties"})
-    local fin_attr = utils.create_lookup({"__class", "__static", "__propget", "__propset"})
-    local fin_class = helper.is_xfc_class_name(class_name) and class_name or (classes[parent].Base or parent)
-    fin_class = helper.xfc_to_fc_class_name(fin_class)
-
-    for k, v in pairs({
-        Methods = {"__class"},
-        StaticMethods = {"__static"},
-        Properties = {"__propget", "__propset"},
-    }) do
-        if result[k] then
-            local ext_attr_test = utils.copy_table_no_meta(ext_attr)
-            local fin_attr_test = utils.copy_table_no_meta(fin_attr)
-
-            ext_attr_test[k] = nil
-            for _, vv in ipairs(v) do
-                fin_attr_test[vv] = nil
-            end
-
-            ext_attr_test = utils.table_keys(ext_attr_test)
-            fin_attr_test = utils.table_keys(fin_attr_test)
-            local test_props = utils.table_keys(result[k])
-
-            local clash_class, clash_attr, clash_name = find_extension_name_clash(result, class_name, parent, ext_attr_test, test_props)
-            if not clash_class then
-                clash_class, clash_attr, clash_name = find_finale_name_clash(fin_class, fin_attr_test, test_props)
-            end
-            if clash_class then
-                return false, "Extension method/property name clash (" .. table.concat({class_name, k, clash_name}, ".") .. "&" .. table.concat({clash_class, clash_attr, clash_name}, ".") .. ")"
             end
         end
     end
